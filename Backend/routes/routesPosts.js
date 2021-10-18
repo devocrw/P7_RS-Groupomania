015298a82b52b23ec42a062b0b93039ceab67b83
devocrw <devocrw@gmail.com>
2021-10-18 11:49:53 +0200

    Correction des routes
    Pour authentification

--- Backend/routes/routesPosts.js
+++ Backend/routes/routesPosts.js
@@ -6,24 +6,24 @@ const multer = require('../middleware/multer-config');
 
 const postCtrl = require('../controllers/post');
 
-try {
-    router.get('/', postCtrl.getAllPosts);
+try{
+    router.get('/', auth, postCtrl.getAllPosts);
     router.post('/', auth, postCtrl.createPost);
     router.put('/:id', auth, postCtrl.updatePost);
-    router.delete('/:id', auth, postCtrl.deletePost);
+    router.delete('/:id', auth, postCtrl.deletePost); 
 
-
+    
     router.get('/likes', auth, postCtrl.getAllLikes);
     router.post('/:id/like', auth, postCtrl.postLike);
-
+ 
     router.get('/:id/comments', auth, postCtrl.getComments);
     router.post('/:id/comments', auth, postCtrl.createComment);
     router.put('/comments/:id', auth, postCtrl.updateComment);
-    router.delete('/comments/:id', auth, postCtrl.deleteComment);
+    router.delete('/comments/:id', auth, postCtrl.deleteComment); 
 
-} catch (error) {
+}catch (error){
     console.log(error);
-}
+} 
 
 module.exports = router;
 
